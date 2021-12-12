fe90c4b91c32d506e08097ec8f22963aeeeefe92
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-11 20:44:41 -0700

    started challenges to practice algorithms

---
+++ 3_length_converter_function.js
@@ -0,0 +1,4 @@
+//todo Creating a function is a skill that'll be useful in many settings, and as you progress, you'll be working on much more complicated functions than this one. But a function that converts units of measure can be pretty handy in multiple professions and industries.
+
+//todo Let's start with a conversion from kilometers to miles. The function should include the input in kilometers and return the answer in miles.
+
